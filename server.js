519d0c029a4d181b19400fe66ecc44d5bafb84fa
Nirmal <nirmalvettiankal@gmail.com>
2018-11-24 23:29:32 -0500

    finish crime counting endpoint

--- server.js
+++ server.js
@@ -14,27 +14,27 @@ app.get('/', (req, res) => {
      req format: {"coords": [{"latitude": 5.12312, "longitude": 5.432532}, {"latitude": 3.2135132, "longitude": 2.132145}]}
      res format: {"crimes": [1, 3]}
  */
-app.get('/api/nearbyCrimes', (req, res) => {
+app.get('/api/nearbyCrimes', async (req, res) => {
 
     const projectId = "hackwestern2018";
     const bigquery = new bq.BigQuery({
         projectId: projectId,
-        keyFilename:"C:\\Users\\nirmal\\Downloads\\hackwestern2018-c0ae9cd3b91b.json"
+        keyFilename: "C:\\Users\\nirmal\\Downloads\\hackwestern2018-c0ae9cd3b91b.json"
     });
 
     const output = [];
     const chicagoLat = 41.8781;
     const chicagoLon = -87.6298;
-    req.coords=[{latitude: chicagoLat, longitude: chicagoLon}];
+    req.coords = [{latitude: chicagoLat, longitude: chicagoLon}];
 
-    req.coords.forEach(coord => {
-        if(!coord.latitude || isNaN(coord.latitude)) {
+    await req.coords.forEach(async coord => {
+        if (!coord.latitude || isNaN(coord.latitude)) {
             res.status(400);
             res.send("Bad latitude coordinate");
             return;
         }
 
-        if(!coord.longitude || isNaN(coord.longitude)) {
+        if (!coord.longitude || isNaN(coord.longitude)) {
             res.status(400);
             res.send("Bad longitude coordinate");
             return;
@@ -46,28 +46,20 @@ app.get('/api/nearbyCrimes', (req, res) => {
         latMin = coord.latitude - 0.0044915;
         latMax = coord.latitude + 0.0044915;
         lonMin = coord.longitude - 0.00753;
-        lonMax = coord.longitude +  0.00753;
+        lonMax = coord.longitude + 0.00753;
 
-        const sqlQuery = `SELECT * FROM \`hackwestern2018.chicago_crime.crime\` WHERE latitude>=${latMin} AND latitude<=${latMax} AND longitude>=${lonMin} AND longitude<=${lonMax} AND date BETWEEN \"2017-11-25\" AND \"2018-11-25\"`;
-        const options = {
-            query: sqlQuery,
-            useLegacySql: false,
-        };
+        const sqlQuery = `SELECT COUNT(*) AS count FROM \`hackwestern2018.chicago_crime.crime\` WHERE latitude>=${latMin} AND latitude<=${latMax} AND longitude>=${lonMin} AND longitude<=${lonMax} AND date BETWEEN \"2017-11-25\" AND \"2018-11-25\"`;
 
-        console.log(sqlQuery);
-        bigquery
-            .query(options)
-            .then(results => {
-                output.push(results);
-                console.log(results);
+        bigquery.createQueryStream(sqlQuery)
+            .on('error', console.error)
+            .on('data', function (row) {
+                output.push(row.count);
+                console.log(row.count);
             })
-            .catch(err => {
-                console.error('ERROR:', err);
+            .on('end', function () {
+                res.json({crimes: output})
             });
     });
-
-
-    res.json({crimes: output})
 
 });
 
