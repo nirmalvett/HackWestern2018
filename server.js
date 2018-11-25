4f037094c208c43905d3dc391c71dfa72a6eca44
Nirmal <nirmalvettiankal@gmail.com>
2018-11-24 22:09:16 -0500

    F

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const bq = require('@google-cloud/bigquery');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+/*
+     Takes a request that contains an array of latitude, longitude coordinates and returns an
+     array of crimes nearby each coordinate
+     req format: {"coords": [{"latitude": 5.12312, "longitude": 5.432532}, {"latitude": 3.2135132, "longitude": 2.132145}]}
+     res format: {"crimes": [1, 3]}
+ */
+app.get('/api/nearbyCrimes', (req, res) => {
+
+    const projectId = "hackwestern2018";
+    const bigquery = new bq.BigQuery({
+        projectId: projectId,
+        keyFilename:"C:\\Users\\nirmal\\Downloads\\hackwestern2018-c0ae9cd3b91b.json"
+    });
+
+    const output = [];
+    const chicagoLat = 41.8781;
+    const chicagoLon = -87.6298;
+    req.coords=[{latitude: chicagoLat, longitude: chicagoLon}];
+
+    req.coords.forEach(coord => {
+        if(!coord.latitude || isNaN(coord.latitude)) {
+            res.status(400);
+            res.send("Bad latitude coordinate");
+            return;
+        }
+
+        if(!coord.longitude || isNaN(coord.longitude)) {
+            res.status(400);
+            res.send("Bad longitude coordinate");
+            return;
+        }
+
+        // Create a 1km box around our latitude/longitude (0.5km radius)
+        // 0.008983 = 1km latitude
+        // 0.015060 = 1km longitude
+        latMin = coord.latitude - 0.0044915;
+        latMax = coord.latitude + 0.0044915;
+        lonMin = coord.longitude - 0.00753;
+        lonMax = coord.longitude +  0.00753;
+
+        const sqlQuery = `SELECT * FROM \`hackwestern2018.chicago_crime.crime\` WHERE latitude>=${latMin} AND latitude<=${latMax} AND longitude>=${lonMin} AND longitude<=${lonMax} AND date BETWEEN \"2017-11-25\" AND \"2018-11-25\"`;
+        const options = {
+            query: sqlQuery,
+            useLegacySql: false,
+        };
+
+        console.log(sqlQuery);
+        bigquery
+            .query(options)
+            .then(results => {
+                output.push(results);
+                console.log(results);
+            })
+            .catch(err => {
+                console.error('ERROR:', err);
+            });
+    });
+
+
+    res.json({crimes: output})
+
+});
+
+const port = 5000;
+app.listen(port, () => `Server running on port ${port}`);
